099b0a722cdd4c4cf36520dc46c753f570f9f7e2
arbaap <arbaadhipamungkas@gmail.com>
2024-06-23 01:29:17 +0700

    pliss

--- storage.js
+++ storage.js
@@ -6,13 +6,23 @@ const dbConfig = require("./mongoDB");
 const encryptedRoute = require("./routes/encryptedRoute");
 const dataRoute = require("./routes/dataRoute");
 
-
-
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const statusCode = 308;
+  if (res.statusCode === statusCode) {
+    const newLocation = "https://test-hum.vercel.app/api/data/kirimData";
+    res.setHeader("Location", newLocation);
+    return res
+      .status(statusCode)
+      .send(`Permanently redirected to ${newLocation}`);
+  }
+  next();
+});
 
 app.get("/", async (req, res, next) => {
   return res.status(200).json({
@@ -23,8 +33,6 @@ app.get("/", async (req, res, next) => {
 
 app.use("/api/encrypted", encryptedRoute);
 app.use("/api/data", dataRoute);
-
-
 
 const port = process.env.PORT || 5000;
 
